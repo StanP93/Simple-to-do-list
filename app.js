2f923b18f48cb29ecf5234af3e2eb30f3432923d
Stanislau Pranovich <stanislaupranovich@MacBook-Air-Stanislau.local>
2021-12-03 11:31:50 +0300

    add buttons with js

--- app.js
+++ app.js
@@ -1,28 +1,56 @@
 let send = document.getElementById('send-to');
 let enter = document.getElementById('enter');
 let taskList = document.getElementById('task-list')
+let firstColumn = document.querySelector('.first-column');
+let inProg = document.getElementById('in-progress');
+let done = document.getElementById('done');
 
-enter.addEventListener('click',function(e) {
-    console.log(enter.value);
-})
+
 
 send.addEventListener('click', function(){
     if(enter.value !== '') {
-        createElem();
-        taskList.deleteElem();
+        taskList.appendChild(createElem());
         enter.value = '';
     }
 })
 
+
+
 function createElem() {
-    let p = document.createElement('p');
-    p.className = "list-elem";
-    p.append(document.createTextNode(enter.value));
-    taskList.appendChild(p);
+    let div = document.createElement('div');
+    div.className = "list-elem";
+    div.append(document.createTextNode(enter.value));
+    div.appendChild(inProgressBtn());
+    div.appendChild(deleteElemBtn());
+    return div;
 }
 
-function deleteElem() {
+function deleteElemBtn() {
     let delBtn = document.createElement('button');
     delBtn.className = "del-btn";
     delBtn.append(document.createTextNode('Delete'));
+    delBtn.addEventListener('click', function(e) {
+        e.path[1].remove();
+    })
+    return delBtn;
+}
+
+function inProgressBtn() {
+    let inProgBtn = document.createElement('button');
+    inProgBtn.className = "prog-btn";
+    inProgBtn.append(document.createTextNode('inProgress'));
+    inProgBtn.addEventListener('click', function(e) {
+        inProg.append(e.path[1]);
+        e.path[0].innerText = 'done';
+        if(e.path[2].id == "in-progress") {
+            done.append(e.path[1]);
+            e.path[0].remove();
+        }
+        console.log(e)
+    })
+    return inProgBtn;
+}
+
+function doneBtn() {
+
 }
